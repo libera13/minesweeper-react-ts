de882a83ca600a77ca8b93bf0b2e8734ae617df9
Emil <emillibera@gmail.com>
2020-07-31 19:40:08 +0200

    Updates

---
+++ precache-manifest.c870099229a6b6931393b258cca2fcd0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232ecf00dc10a66c6cc2dc66c16abc40",
+    "url": "/MineSweeper/index.html"
+  },
+  {
+    "revision": "90ff021c2e43237d0a3e",
+    "url": "/MineSweeper/static/css/main.0b4f42e8.chunk.css"
+  },
+  {
+    "revision": "a16ae8b3edb89d03e1e0",
+    "url": "/MineSweeper/static/js/2.1bac962e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MineSweeper/static/js/2.1bac962e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ff021c2e43237d0a3e",
+    "url": "/MineSweeper/static/js/main.471fa31d.chunk.js"
+  },
+  {
+    "revision": "3b976933abca7dfbd054",
+    "url": "/MineSweeper/static/js/runtime-main.ea63a830.js"
+  },
+  {
+    "revision": "a68d62faffda388095ec72d6fc4ec51d",
+    "url": "/MineSweeper/static/media/FaceLose.a68d62fa.png"
+  },
+  {
+    "revision": "2180eab2ae8ff668a0ecbc14d20f1abe",
+    "url": "/MineSweeper/static/media/mine.2180eab2.png"
+  }
+]);
